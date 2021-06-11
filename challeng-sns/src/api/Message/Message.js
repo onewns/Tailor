958f2e36486dfb6fc9b3a982b69ead191af89420
wonjun9090 <wonjun9090@naver.com>
2021-06-11 14:39:42 +0900

    Run apollo server

--- challeng-sns/src/api/Message/Message.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../../../generated/prisma-client";
-
-export default {
-  Message: {
-    from: ({ id }) => prisma.message({ id }).from(),
-    to: ({ id }) => prisma.message({ id }).to(),
-    room: ({ id }) => prisma.message({ id }).room()
-  }
-};
